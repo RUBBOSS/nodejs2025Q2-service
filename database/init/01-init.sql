d21bf5a9f10074080c08b5c2c9ba3c15c3cc6d09
Ruben Martirosyan <rubboss1992@gmail.com>
2025-06-13 13:28:48 +0400

    Fix Docker Compose startup issues
    - Resolve network subnet conflict by changing from 172.20.0.0/16 to 172.21.0.0/16
    - Synchronize database credentials across all config files (home_library/rub54321)
    - Update docker-compose.yml environment variables for consistent auth
    - Update Dockerfile.postgres ENV variables to match docker-compose
    - Update database init scripts to use correct database name
    - Remove obsolete docker-compose version field
    - All containers now start successfully and connect properly
    - Database migrations executed and all tests passing

--- database/init/01-init.sql
+++ database/init/01-init.sql
@@ -2,4 +2,4 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 -- Schema will be created by NestJS/TypeORM migrations
 
-COMMENT ON DATABASE nodejs2025q2 IS 'Database for nodejs2025q2 NestJS service';
+COMMENT ON DATABASE home_library IS 'Database for nodejs2025q2 NestJS service';
